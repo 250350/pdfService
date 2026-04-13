4dbac7d9f149e00dd2d49d8c3595b11c28572f8d
250350 <piechotapoczta@wp.pl>
2026-04-13 21:25:47 +0200

    Changes for deployment

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require("express");
-const puppeteer = require("puppeteer-core");
-const executablePath = process.env.PUPPETEER_EXECUTABLE_PATH;
+const puppeteer = require("puppeteer");
 
 const app = express();
-
-app.use((req, res, next) => {
-    console.log("➡️", req.method, req.url);
-    next();
-});
 
 app.use(express.json({ limit: "50mb" }));
 
 app.post("/pdf", async (req, res) => {
     try {
-        console.log("BEFORE PDF");
         const { html } = req.body;
+
+        console.log("➡️ POST /pdf");
+        console.log("BEFORE PDF");
 
         const browser = await puppeteer.launch({
             headless: "new",
-            executablePath,
             args: [
                 "--no-sandbox",
                 "--disable-setuid-sandbox",
@@ -33,26 +28,22 @@ app.post("/pdf", async (req, res) => {
             waitUntil: "networkidle0",
             timeout: 60000
         });
-        console.log("HTML RECEIVED LENGTH:", html.length);
-
-        await page.emulateMediaType("print");
 
         const pdf = await page.pdf({
             format: "A4",
-            printBackground: true,
+            printBackground: true
         });
+
         console.log("PDF GENERATED:", pdf.length);
 
         await browser.close();
-        console.log("PDF SIZE:", pdf.length);
 
         res.setHeader("Content-Type", "application/pdf");
         res.send(pdf);
-        console.log("AFTER PDF");
 
     } catch (e) {
         console.error("PDF ERROR FULL:", e);
-        res.status(500).send("PDF ERROR: " + e.message);
+        res.status(500).send(e.message);
     }
 });
 
